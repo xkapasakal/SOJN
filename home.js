2d8478f4130a286a028c948e05a7ab8344ac30e6
Christos Kapasakalidis <xkapasakal@gmail.com>
2013-08-02 17:33:29 +0300

    closures...

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+"use strict";
+
 window.onload = function (event){
 	var values = [ 213, 16, 2058, 54, 10, 1965, 57, 9 ];
 	var sortedValues = values.sort(function(value1, value2){return value1 - value2});
@@ -7,3 +9,61 @@ window.onload = function (event){
 
 function isNimble(){ return true; }
 
+function fun() { return this; }
+fun();
+fun.call(2);
+fun.apply(null);
+fun.call(undefined);
+fun.bind(true);
+var fvar = fun;
+fvar();
+
+
+document.writeln("A: " + this);
+
+var o = {
+	f: function() {
+		return this;
+	}
+};
+document.writeln("B: " + o.f());
+var f = o.f;
+document.writeln("C: " + f());
+
+var obj = {};
+document.writeln("D: " + f.call(obj));
+
+var o = {
+	f: function() {
+		var ff = function() {
+			return this;
+		};
+		return ff();
+	},
+	g: {
+		h: function() {
+			return this;
+		}
+	}
+};
+document.writeln("E: " + o.f());
+document.writeln("F: " + o.g.h());
+
+
+var outerValue = 'ninja';
+
+var later;
+
+function outerFunction() {
+  var innerValue = 'samurai';
+  function innerFunction() {
+    console.log(outerValue);
+    console.log(innerValue);
+  }
+  later = innerFunction;
+}
+
+outerFunction();
+
+later();
+ 
